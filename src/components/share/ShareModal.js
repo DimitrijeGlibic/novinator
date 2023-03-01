f879db563abf0ed04e5cae2a6d159fc70fa9ab40
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2023-03-01 23:17:50 +0100

    Style ShareModal

--- src/components/share/ShareModal.js
+++ src/components/share/ShareModal.js
@@ -1,23 +1,42 @@
 import { Button, Modal } from "react-bootstrap";
+import styled from "styled-components";
 
 const ShareModal = ({ isVisible, handleCloseModal }) => {
   return (
-    <Modal show={isVisible} onHide={handleCloseModal} backdrop="static" keyboard={false}>
-      <Modal.Header closeButton>
+    <Modal
+      show={isVisible}
+      onHide={handleCloseModal}
+      keyboard={false}
+    >
+      <StyledHeader>
         <Modal.Title>Spread the word about this post</Modal.Title>
-      </Modal.Header>
+      </StyledHeader>
       <Modal.Body>
-        I will not close if you click outside me. Don't even try to press escape
-        key.
+        <div className="d-grid gap-2 mt-4">
+          <Button variant="outline-secondary mt-3">Instagram</Button>
+        </div>
+        <div className="d-grid gap-2">
+          <Button variant="outline-secondary mt-3">Facebook</Button>
+        </div>
+        <div className="d-grid gap-2">
+          <Button variant="outline-secondary mt-3">Twiter</Button>
+        </div>
+        <div className="d-grid gap-2">
+          <Button variant="outline-secondary mt-3">Whatsapp</Button>
+        </div>
+        <div className="d-grid gap-2 mt-5">
+          <Button variant="outline-success">Get share link</Button>
+        </div>
+        <div className="d-grid gap-2">
+          <Button variant="outline-primary mt-3">Share on Novinator</Button>
+        </div>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleCloseModal}>
-          Close
-        </Button>
-        <Button variant="primary">Understood</Button>
-      </Modal.Footer>
     </Modal>
   );
 };
 
 export default ShareModal;
+
+const StyledHeader = styled(Modal.Header)`
+    border-bottom: 0;
+`;
