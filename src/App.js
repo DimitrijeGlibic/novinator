fd17fd4ebe093618433321f5466de958ffa38d73
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2023-03-01 23:38:14 +0100

    Install react-router-dom and add routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import Container from "react-bootstrap/Container";
 import Feed from "./feed/Feed";
+import { Route, Routes } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "swiper/css";
 import "./App.css";
-import { Fragment } from "react";
-import styled from "styled-components";
-import MenuSideDrawer from "./components/menu/MenuSideDrawer";
+
 function App() {
   return (
-    <Fragment>
-      <FeedContainer>
-        <MenuSideDrawer />
-        <Feed />
-      </FeedContainer>
-    </Fragment>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+      </Routes>
   );
 }
 
 export default App;
 
-const FeedContainer = styled(Container)`
-  @media (max-width: 576px) {
-    padding: 0;
-  }
-`;
--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -2,6 +2,8 @@ import Post from "../components/post/Post";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { data } from "./dumyData";
 import styled from "styled-components";
+import MenuSideDrawer from "../components/menu/MenuSideDrawer";
+import { Container } from "react-bootstrap";
 
 const Feed = () => {
   const renderPosts = () => {
@@ -13,23 +15,32 @@ const Feed = () => {
   };
 
   return (
-    <Swiper
-      spaceBetween={0}
-      slidesPerView={1}
-      direction="vertical"
-      style={{height: "100vh"}}
-      mousewheel={true}
-    >
-      {renderPosts()}
-      <SwiperSlide>
-        <FeedEnd>End of feed, Click to go back to top</FeedEnd>
-      </SwiperSlide>
-    </Swiper>
+    <FeedContainer>
+      <MenuSideDrawer />
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        direction="vertical"
+        style={{ height: "100vh" }}
+        mousewheel={true}
+      >
+        {renderPosts()}
+        <SwiperSlide>
+          <FeedEnd>End of feed, Click to go back to top</FeedEnd>
+        </SwiperSlide>
+      </Swiper>
+    </FeedContainer>
   );
 };
 
 export default Feed;
 
 const FeedEnd = styled.div`
-    height: 10vh;
+  height: 10vh;
+`;
+
+const FeedContainer = styled(Container)`
+  @media (max-width: 576px) {
+    padding: 0;
+  }
 `;
