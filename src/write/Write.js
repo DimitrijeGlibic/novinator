472affcd6a378923cbcfd7a6ad28a192ff986de7
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2023-03-04 23:54:03 +0100

    Rearange Write components

--- src/write/Write.js
+++ src/write/Write.js
@@ -14,16 +14,16 @@ const Write = () => {
 
   return (
     <WriteWrapper>
-      <ThemeChoserWrapper>
-        <ThemeChoser />
-      </ThemeChoserWrapper>
       <Swiper
         spaceBetween={16}
         slidesPerView={1}
-        style={{ height: "100%", width: "100%" }}
+        style={{ width: "100%" }}
       >
         {renderSlidesPreview()}
       </Swiper>
+      <ThemeChoserWrapper>
+        <ThemeChoser />
+      </ThemeChoserWrapper>
       <div className="d-grid gap-2 mt-4">
           <PublishButton variant="warning">Publish</PublishButton>
         </div>
@@ -36,27 +36,20 @@ export default Write;
 const WriteWrapper = styled.div`
   background-color: green;
   padding: 100px 24px;
-  height: 100vh;
   position: relative;
+  height: 100vh;
 `;
 
 const SlidePreview = styled.div`
   width: 100%;
-  height: 100%;
+  height: 60vh;
   background-color: red;
   border-radius: 6px;
 `;
 
 const PublishButton = styled(Button)`
-  position: absolute;
-  bottom: 24px;
-  left: 24px;
-  right: 24px;
 `;
 
 const ThemeChoserWrapper = styled.div`
-  position: absolute;
-  right: 24px;
-  left: 80px;
-  top: 32px;
+  margin-top: 32px;
 `;
