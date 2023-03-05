07c6374060ccad6369acd1ac78a983d2114f3a1e
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2023-03-05 01:28:06 +0100

    Add editable heading for first slide

--- src/write/SlideCreator/FirstSlidePreview.js
+++ src/write/SlideCreator/FirstSlidePreview.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+
 import styled from "styled-components";
 
 const FirstSlidePreview = ({ theme: { background, titleColor, font } }) => {
-    return (
-        <SlideWrapper $background={background}>
-            <StyledHeading $color={titleColor} $font={font}>
-                Upisi naslov
-            </StyledHeading>
-        </SlideWrapper>
-    );
+  const [currentTitle, setCurrentTitle] = useState("Upisi naslov posta");
+  const [currentRows, setCurrentRows] = useState(1);
+
+  const handleChange = (e) => {
+    setCurrentRows( Math.floor(e.target.scrollHeight / 32.88));
+  }
+
+  return (
+    <SlideWrapper $background={background}>
+      <Form>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <StyledInputHeading
+            as="textarea"
+            type="text"
+            placeholder="Upisi naslov posta"
+            $color={titleColor}
+            $font={font}
+            onChange={handleChange}
+            rows={currentRows}
+          />
+        </Form.Group>
+      </Form>
+    </SlideWrapper>
+  );
 };
 
 export default FirstSlidePreview;
@@ -24,7 +44,28 @@ const SlideWrapper = styled.div`
 `;
 
 const StyledHeading = styled.h1`
+  color: ${({ $color }) => $color};
+  font-weight: bold;
+  font-family: ${({ $font }) => $font};
+`;
+
+const StyledInputHeading = styled(Form.Control)`
+  width: 100%;
+  color: ${({ $color }) => $color};
+  font-weight: bold;
+  font-family: ${({ $font }) => $font};
+  padding: 0;
+  background: transparent;
+  border: 0;
+  font-size: 27.4px;
+  line-height: 32.88px;
+  &:focus {
+    padding: 0;
+    background: transparent;
+    border: 0;
     color: ${({ $color }) => $color};
     font-weight: bold;
     font-family: ${({ $font }) => $font};
+    box-shadow: none;
+  }
 `;
