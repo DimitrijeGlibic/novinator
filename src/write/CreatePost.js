7ad337aaf5c926da68f409cc2c15c49a8bf94953
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2023-05-13 14:35:32 +0200

    Fix Crate story SlideWrapper
    Similar to commit 7bdb46b

--- src/write/CreatePost.js
+++ src/write/CreatePost.js
@@ -69,8 +69,12 @@ export const CreatePost = ({ themes }) => {
 export default CreatePost;
 
 const SlideWrapper = styled.div`
-  height: 100vh;
-  position: relative;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `;
 
 const PublishButton = styled(Button)`
