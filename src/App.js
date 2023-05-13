ccae140e2d3c74556358b58ce94b7bfae83ada28
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2023-05-13 14:32:00 +0200

    Delete Write.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "swiper/css";
 import "./App.css";
 import TimeLine from "./timeline/TimeLine";
 import SignInModal from "./components/menu/SignIn/SignInModal";
-import Write from "./write/Write";
 import MenuSideDrawer from "./components/menu/MenuSideDrawer";
 import { Fragment } from "react";
 import CreatePost from "./write/CreatePost";
@@ -20,7 +19,6 @@ function App() {
         <Route path="/:postId" element={<TimeLine />}>
           <Route path="/:postId/sign-in" element={<SignInModal />} />
         </Route>
-        <Route path="/write" element={<Write />} />
         <Route path="/create-post" element={<CreatePost themes={themes} />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
